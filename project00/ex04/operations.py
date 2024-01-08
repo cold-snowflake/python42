e4417b7e3d06682e4807e26e2b530441255fe65a
cold-snowflake <cold.snowflake01@gmail.com>
2024-01-08 22:19:40 +0100

    ex04

--- project00/ex04/operations.py
+++ project00/ex04/operations.py
@@ -1,9 +1,19 @@
 import sys
+if len(sys.argv) == 3:
+    sum = int(sys.argv[1]) + int(sys.argv[2])
+    product = int(sys.argv[1]) * int(sys.argv[2])
+    difference = int(sys.argv[1]) - int(sys.argv[2])
 
+if len(sys.argv) == 3 and int(sys.argv[2]) != 0:
+    quotient = int(sys.argv[1]) / int(sys.argv[2])
+    remainder = int(sys.argv[1]) % int(sys.argv[2])
+else:
+    quotient = " ERROR (division by zero)"
+    remainder = " ERROR (modulo by zero)"
 print(f"""
-    Sum: {int(sys.argv[1]) + int(sys.argv[2])}
-    Difference: {int(sys.argv[1]) - int(sys.argv[2])}
-    Product: {int(sys.argv[1]) * int(sys.argv[2])}
-    Quotient: {int(sys.argv[1]) / int(sys.argv[2])}
-    Remainder: {int(sys.argv[1]) % int(sys.argv[2])}
+    Sum: {sum}
+    Difference: {difference}
+    Product: {product}
+    Quotient: {quotient}
+    Remainder: {remainder}
 """)
