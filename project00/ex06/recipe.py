a96614984e52546be705d7f9d6286daa32907a18
cold-snowflake <cold.snowflake01@gmail.com>
2024-01-27 21:22:25 +0100

    recipe part2

---
+++ project00/ex06/recipe.py
@@ -0,0 +1,73 @@
+# --------------------------------------------------------------------------------
+# : color code
+RED = '\033[91m'
+GRE = '\033[92m'
+YEL = '\033[93m'
+BLU = '\033[94m'
+MAG = '\033[95m'
+CYA = '\033[96m'
+RES = '\033[0m'
+BOL = '\033[1m'
+UND = '\033[4m'
+
+# --------------------------------------------------------------------------------
+cookboock = {
+    "sandwich":
+    {
+        "ingredients": ["ham", "bread", "cheese", "tomatoes"],
+        "meal": "lunch",
+        "prep_time": 10
+    },
+    "cake": 
+    {
+        "ingredients": ["flour", "sugar", "eggs"],
+        "meal": "dessert",
+        "prep_time": 60
+    },
+    "salad":
+    {
+        "ingredients": ["avocado", "arugula", "tomatoes", "spinach"],
+        "meal": "salad",
+        "prep_time": 15
+    }
+}
+
+# --------------------------------------------------------------------------------
+#print all recipe names
+def print_all_name_of_recipe(recipe_dic):
+    print(f"{BOL}{RED}NAME OF RECIPE:{RES}")
+    for key in recipe_dic:
+        print(f"{MAG}{BOL}{key}{RES}",)
+
+# print_all_name_of_recipe(cookboock)
+
+# --------------------------------------------------------------------------------
+#function that takes a recipe name and print its details
+
+name_of_recipe = input(f"{CYA}{BOL}Enter name of food:{RES} ")
+
+def print_recipe_details(name_of_recipe):
+    try:
+        val = cookboock.get(name_of_recipe.lower())
+        print(f"{BLU}{BOL}{name_of_recipe.upper()}{RES}\n{YEL}{UND}Ingredients{RES}: {val['ingredients']}")
+        print(f"{BLU}To be eaten for {val['meal']}.{RES}")
+        print(f"Takes {val['prep_time']} minutes of cooking.")
+    except TypeError:
+        print(f"{RED}{BOL}We don`t have a such recipe!{RES}")
+
+# print_recipe_details(name_of_recipe)
+
+# --------------------------------------------------------------------------------
+#function that takes a recipe name and delete it.
+
+name_of_food = input(f"{BLU}{BOL}Enter name of food for deletion: {RES}")
+
+def delete_cook(name_of_food):
+    val = cookboock.get(name_of_food.lower())
+    try:
+        val.clear()
+        print(f"{RED}{BOL}{name_of_food.lower()} deleted{RES}")
+    except AttributeError:
+        print(f"{RED}{BOL}We don`t have a such recipe!{RES}")
+
+# delete_cook(name_of_food)
